cc22d21c4ca61f2032b9abe1cb668df5bf52b41f
Sakshi Gautam <gautamsakshi551@gmail.com>
2026-02-17 16:11:04 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("SaaS Production App Running ");
+  res.send("SaaS Production App Running 🚀");
 });
 
 app.get("/health", (req, res) => {
