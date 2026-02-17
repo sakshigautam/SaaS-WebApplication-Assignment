a4dc2a004d694a8129e3dc8c4fb7212c21ec722b
Sakshi Gautam <gautamsakshi551@gmail.com>
2026-02-17 10:31:40 +0530

    Add security groups for ALB and EC2 instances

---
+++ terraform/modules/security/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "alb" {
+  name   = "alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name   = "ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+}
