384cb5140065bbf4142593fe334b73b89d283286
Sakshi Gautam <gautamsakshi551@gmail.com>
2026-02-18 11:30:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-
-
 module "vpc" {
   source          = "../../modules/vpc"
   cidr            = var.vpc_cidr
@@ -12,33 +10,26 @@ module "vpc" {
   azs             = var.azs
 }
 
-
-
 module "security" {
   source = "../../modules/security"
   vpc_id = module.vpc.vpc_id
 }
-
-
 
 module "alb" {
   source          = "../../modules/alb"
   vpc_id          = module.vpc.vpc_id
   public_subnets  = module.vpc.public_subnet_ids
   alb_sg          = module.security.alb_sg_id
-  acm_cert        = "your-acm-certificate-arn"
+  acm_cert = "arn:aws:acm:us-east-1:123456789012:certificate/7b1c2d34-56ef-78ab-90cd-ef1234567890"
 }
-
-
 
 module "compute" {
-  source            = "../../modules/compute"
-  private_subnets   = module.vpc.private_subnet_ids
-  target_group_arn  = module.alb.target_group_arn
-  ami               = "ami-xxxxxxxx"
-  instance_profile  = "your-ec2-instance-profile"
+  source           = "../../modules/compute"
+  private_subnets  = module.vpc.private_subnet_ids
+  target_group_arn = module.alb.target_group_arn
+  ami = "ami-0c02fb55956c7d316"
+  instance_profile = "ec2-saas-prod-instance-profile"
 }
-
 
 module "monitoring" {
   source         = "../../modules/monitoring"
@@ -47,10 +38,8 @@ module "monitoring" {
   sns_email      = var.sns_email
 }
 
-
 module "budget" {
   source       = "../../modules/budget"
   email        = var.budget_email
   limit_amount = "100"
 }
-
