578328ce77da69cf9b9bb649efb684083edf539e
Sakshi Gautam <gautamsakshi551@gmail.com>
2026-02-18 11:27:53 +0530

    Update user_data.sh to use ECR and systemctl

--- terraform/modules/compute/user_data.sh
+++ terraform/modules/compute/user_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum update -y
 amazon-linux-extras install docker -y
-service docker start
-docker run -d -p 80:3000 your-dockerhub/sample-app:latest
+systemctl start docker
+docker pull 1268976533561.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
+docker run -d -p 80:3000 1268976533561.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
