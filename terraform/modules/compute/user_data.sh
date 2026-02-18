17c61d4fade3fb50e0471f515791c5b08c41e3fa
Sakshi Gautam <gautamsakshi551@gmail.com>
2026-02-18 11:31:27 +0530

    Update user_data.sh

--- terraform/modules/compute/user_data.sh
+++ terraform/modules/compute/user_data.sh
@@ -2,5 +2,5 @@
 yum update -y
 amazon-linux-extras install docker -y
 systemctl start docker
-docker pull 1268976533561.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
-docker run -d -p 80:3000 1268976533561.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
+docker pull 123456789012.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
+docker run -d -p 80:3000 123456789012.dkr.ecr.us-east-1.amazonaws.com/saas-app:latest
